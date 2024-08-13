643e31ac9d66c2af27e968b7628b96bfdb3bc871
Jorge Castro <jorge.castro.rivero@gmail.com>
2024-08-13 20:11:49 +0200

    Created python file for parsing and validating

---
+++ parse_validate.py
@@ -0,0 +1,13 @@
+from rdflib import Graph
+from pyshacl import validate
+
+graph = Graph()
+graph.namespace_manager.bind('xsd', 'http://www.w3.org/2001/XMLSchema#')
+graph.parse('p_kg.ttl', format='turtle')
+
+shacl = Graph()
+shacl.namespace_manager.bind('xsd', 'http://www.w3.org/2001/XMLSchema#')
+shacl.parse('p_shacl.shacl')
+
+r = validate(graph, shacl_graph=shacl, inference='rdfs', meta_shacl=False, debug=False)
+conforms, results_graph, results_text = r
